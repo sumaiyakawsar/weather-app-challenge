b194da858fba01e1911eaf3dbf8c69839ea1203a
Sumaiya Kawsar <sumaiya.kawsar693@gmail.com>
2025-09-07 17:11:38 +0300

    Set up the project

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState, useEffect } from "react";
+ 
+function App() {
+  
+  return (
+
+    <main className="weather-app">
+      <div className="container">
+        
+      </div>
+    </main>
+  )
+}
+
+export default App
