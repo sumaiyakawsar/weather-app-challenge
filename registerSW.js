235f657e2321145fb6c985f2d214af3db057cb15
Sumaiya Kawsar <sumaiya.kawsar693@gmail.com>
2025-09-28 22:20:06 +0300

    Updates

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('./sw.js', { scope: './' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/weather-app-challenge/sw.js', { scope: '/weather-app-challenge/' })})}
